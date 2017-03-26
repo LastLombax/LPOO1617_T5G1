41cadbe15e2d06a556ee10379e9ead766d243424
Vicente Espinha <up201503764@fe.up.pt>
2017-03-26 16:57:55 +0100

    started javadoc

--- Guided_Project_LPOO/src/dkeep/logic/Character.java
+++ Guided_Project_LPOO/src/dkeep/logic/Character.java
@@ -3,16 +3,68 @@ package dkeep.logic;
 import java.io.Serializable;
 
 public interface Character extends Serializable{  
-	   
-	public int getCoordenateI(); //returns the coordinate i of the character
-	public int getCoordenateJ(); //returns the coordinate j of the character
-	public char getSprite(); //returns the sprite of the character
-	public void setCoordenateI(int i); //sets the coordinate i of the character
-	public void setCoordenateJ(int j); //sets the coordinate j of the character
-	public void setSprite(char s); //sets the sprite of the character
-	public boolean hasClub(); //returns true if the character has a club
-	public Club getClub(); //returns the club of the character
-	public int getStun(); //returns the turn in which the character is stunned
-	public void setStun(int x);	 //sets the character with the stun turn x
-	public void move(); //moves the character
+	/**  
+	 * returns part of the character position 
+	 * @return int with the coordinate i of the character.  
+	 */   
+	public int getCoordenateI();
+	
+	/**  
+	 * returns part of the character position 
+	 * @return int with the coordinate j of the character.  
+	 */ 
+	public int getCoordenateJ();
+	
+	/**  
+	 * returns sprite of the character 
+	 * @return char with the sprite of the character.  
+	 */ 
+	public char getSprite();
+	
+	/**  
+	 * Sets the coordinate i of the character 
+	 * @param i int with the coordinate i of the character.  
+	 */ 
+	public void setCoordenateI(int i);
+	
+	/**  
+	 * Sets the coordinate j of the character 
+	 * @param j int with the coordinate j of the character.  
+	 */ 
+	public void setCoordenateJ(int j);
+	
+	/**  
+	 * Sets the sprite of the character 
+	 * @param s char with the sprite of the character.  
+	 */ 
+	public void setSprite(char s);
+	
+	/**  
+	 * returns a boolean to check if has a Club or not
+	 * @return true if has a Club, false if dont. 
+	 */ 
+	public boolean hasClub();
+	
+	/**  
+	 * returns a Club
+	 * @return Club of the character. 
+	 */ 
+	public Club getClub();
+	
+	/**  
+	 *  returns the turn in which the character is stunned
+	 * @return  the turn in which the character is stunned. 
+	 */ 
+	public int getStun();
+	
+	/**  
+	 *  sets the character with the stun turn x
+	 * @param  x int the turn in which the character is stunned. 
+	 */ 
+	public void setStun(int x);
+	
+	/**  
+	 *  moves the character 
+	 */ 
+	public void move();
 }
--- Guided_Project_LPOO/src/dkeep/logic/GameMap.java
+++ Guided_Project_LPOO/src/dkeep/logic/GameMap.java
@@ -4,20 +4,70 @@ import java.util.Vector;
   
 public abstract class GameMap implements Serializable { 
 	
-	public abstract Hero getHero();  //returns Hero with the initial coordinates
-	public abstract Vector<Ogre> getOgres(); //returns Ogres with the initial coordinates
-	public abstract Vector<Club> getClubs(); 
-	public abstract Guard getGuard();  //returns Guard with the initial coordinates
-	public abstract Lever getLever(); //returns Lever of map	
-	public abstract Key getKey(); //returns key of map
-	public abstract Vector<Exit> getExits(); //returns all the exits existing in the map
-	public abstract Club getHeroClub(); //returns the hero club with the initial coordinates
-
-	public abstract char[][] getMap();	//returns the map (char[][])
+	/**  
+	 * returns Hero with the initial coordinates
+	 * @return Hero 
+	 */ 
+	public abstract Hero getHero();
 	
+	/**  
+	 * returns Ogres with the initial coordinates
+	 * @return Vector<Ogre>ogres 
+	 */
+	public abstract Vector<Ogre> getOgres();
+	
+	/**  
+	 * returns Clubs with the initial coordinates
+	 * @return Vector<Club>clubs
+	 */
+	public abstract Vector<Club> getClubs();
+	
+	/**  
+	 * returns Guard with the initial coordinates
+	 * @return Guard
+	 */
+	public abstract Guard getGuard();
+	
+	/**  
+	 * returns Lever of map
+	 * @return Lever
+	 */
+	public abstract Lever getLever();
+	
+	/**  
+	 *returns key of map
+	 * @return Key
+	 */
+	public abstract Key getKey();
+	
+	/**  
+	 *returns all the exits existing in the map
+	 * @return Vector<Exit>exits
+	 */
+	public abstract Vector<Exit> getExits();
+	
+	/**  
+	 *returns the hero club with the initial coordinates
+	 * @return Club
+	 */
+	public abstract Club getHeroClub();
+
+	/**  
+	 *returns the map (char[][])
+	 * @return char[][]
+	 */
+	public abstract char[][] getMap();
+	
+	/**  
+	 *checks if the position with coordinates [i][j] of the map are valid (not door or wall)
+	 * @return true if valid position,else false
+	 */
 	public abstract boolean validPos(int x, int y); //checks if the position with coordinates [i][j] of the map are valid (not door or wall)
 	
-	public abstract boolean hasGuard();	//returns true if the level has a Guard,false if not
+	/**  
+	 *returns true if the level has a Guard,false if not
+	 */
+	public abstract boolean hasGuard();
 	public abstract boolean hasOgre();	//returns true if the level has a Ogre,false if not
 	public abstract boolean hasCLub();   //returns true if the Ogre has a Club,false if not
 	public abstract boolean hasKey();	//returns true if the level has a key,false if not
